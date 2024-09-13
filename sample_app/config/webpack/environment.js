92bf87a5981b4c08d64a969f81f0635926e94083
MyNguyen12345 <mynguyen04012000@gmail.com>
2024-09-13 16:23:37 +0700

    logging in

--- sample_app/config/webpack/environment.js
+++ sample_app/config/webpack/environment.js
@@ -2,4 +2,11 @@ const { environment } = require('@rails/webpacker')
 environment.config.merge({
     node: false,
 })
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+new webpack.ProvidePlugin({
+$: 'jquery/src/jquery',
+jQuery: 'jquery/src/jquery'
+})
+)
 module.exports = environment
